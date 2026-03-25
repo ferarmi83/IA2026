d8db843fc247f775fa015235112dc6ddcd4e9637
Fernando Aramayo Michel <ferarmi83@gmail.com>
2026-03-25 08:09:53 -0400

    Agregar archivos de proyecto.

---
+++ IA2026/FRMOchoPuzzle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IA2026
+{
+    public partial class FRMOchoPuzzle : Form
+    {
+        public FRMOchoPuzzle()
+        {
+            InitializeComponent();
+        }
+
+        private void LBL00_Click(object sender, EventArgs e)
+        {
+            if (LBL10.Text == "0")
+            {
+                LBL10.Text = LBL00.Text;
+                LBL00.Text = "0";
+            }
+            else if (LBL01.Text == "0")
+            {
+                LBL01.Text = LBL00.Text;
+                LBL00.Text = "0";
+            }
+        }
+
+        private void LBL10_Click(object sender, EventArgs e)
+        {
+            if (LBL00.Text == "0")
+            {
+                LBL00.Text = LBL10.Text;
+                LBL10.Text = "0";
+            }
+            else if (LBL11.Text == "0")
+            {
+                LBL11.Text = LBL10.Text;
+                LBL10.Text = "0";
+            }
+            else if (LBL20.Text == "0")
+            {
+                LBL20.Text = LBL10.Text;
+                LBL10.Text = "0";
+            }
+        }
+    }
+}
